d20372608b918e40d51a29ba4608e684c113119e
[z1one516] <z1one5156@gmail.com>
2023-04-14 15:39:21 +0900

    Add tel and modify btn1 in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-                             QMessageBox)
+                             QMessageBox, QPlainTextEdit)
 from PyQt5.QtGui import QIcon
 
 
@@ -11,13 +11,15 @@ class Calculator(QWidget):
         self.initUI()
         
     def initUI(self):
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+        
         self.btn1=QPushButton('Message',self)
         self.btn1.clicked.connect(self.activateMessage)
         
         vbox=QVBoxLayout()
-        vbox.addStretch(1)
+        vbox.addWidget(self.te1)
         vbox.addWidget(self.btn1)
-        vbox.addStretch(1)
         self.setLayout(vbox)
         
         self.setWindowTitle('Calculator')
@@ -26,7 +28,7 @@ class Calculator(QWidget):
         self.show()
         
     def activateMessage(self):
-        QMessageBox.information(self,"information","Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
         
         
 if __name__ =='__main__':
